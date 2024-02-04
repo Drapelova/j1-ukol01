2d2fcba795396923e9948f219a49ccf409c7c400
Natálie Drápelová <simonova.natla@gmail.com>
2024-02-04 12:40:31 +0100

    Nakresli Prasátko

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -13,6 +13,74 @@ public class HlavniProgram {
     zofka = new Turtle();
 
     //TODO implementace domácího úkolu
+
+  nakresliPrasatko();
   }
 
-}
+
+    /*zofka.penUp();
+    zofka.turnLeft(90);
+    zofka.move(200);
+    zofka.turnRight(90);
+    zofka.penDown();
+
+    nakresliCtverecek();
+  }
+ /* public void nakresliCtverecek() {
+        for (int i = 0; i < 4; i++) {
+          zofka.move(100);
+          zofka.turnRight(90);
+
+  */
+  public void nakresliObdelnik() {
+
+    zofka.turnRight(90);
+    zofka.move(200);
+    zofka.turnLeft(90);
+    zofka.move(150);
+    zofka.turnLeft(90);
+    zofka.move(200);
+    zofka.turnLeft(90);
+    zofka.move(150);
+
+  }
+
+  public void nakresliTrojuhelnik() {
+    zofka.turnRight(120);
+    zofka.move(150);
+    zofka.turnRight(120);
+    zofka.move(150);
+    zofka.turnRight(120);
+    zofka.move(150);
+    }
+
+    public void nakresliNozicky() {
+    zofka.turnRight(60);
+    zofka.move(80);
+    zofka.penUp();
+    zofka.turnLeft(180);
+    zofka.move(80);
+    zofka.penDown();
+      zofka.turnRight(60);
+      zofka.move(80);
+      zofka.turnLeft(180);
+      zofka.move(80);
+    }
+
+      public void nakresliPrasatko() {
+        nakresliObdelnik();
+        nakresliTrojuhelnik();
+        nakresliNozicky();
+        zofka.turnRight(150);
+        zofka.move(200);
+        zofka.turnRight(90);
+        nakresliNozicky();
+    }
+    }
+
+
+
+
+
+
+
